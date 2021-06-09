ca3982470167409b3b837b1f720ec66a73f13335
Bayekula-hb <hobedbayekula@gmail.com>
2021-06-09 19:24:59 +0100

    refactor change the picture of the section home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import Header from "./components/Header";
-import Photo1 from "./Public/Image/handsome-man-reading-book.jpg"
+import Photo1 from "./Public/Image/students.jpg";
 
-const App = ()=>{
-
-  return(
+const App = () => {
+  return (
     <>
       <Header />
       <section id="Home" className="section section--home">
         <div className="col-12 row div--home">
-          <img src={Photo1} alt="étudiant" className="col-6 img--home"/>
+          <div className="col-4 container-img">
+            <img src={Photo1} alt="étudiant" className="img--home" />
+          </div>
           <div className="col-6 div--text">
-            <p className="para--apprendre">
-              L'apprentissage à votre porte
-            </p>
+            <p className="para--apprendre">L'apprentissage à votre porte</p>
             <p className="para--details">
-              Désormais vous pouvez apprendre quand vous voulez, à n'importe quel moment
-              , et où vous voulez
+              Désormais vous pouvez apprendre quand vous voulez, à n'importe
+              quel moment , et où vous voulez
             </p>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 export default App;
