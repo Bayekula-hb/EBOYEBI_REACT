7da3ad7910d2d498f1f1e2c94ac72d8256f10d09
Bayekula-hb <hobedbayekula@gmail.com>
2021-06-16 22:49:29 +0100

    refactor mise en commentaire de useeffect dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-// import axios from "axios";
-import { useEffect } from "react";
+
 import Header from "./components/Header";
-import Query from "./components/query";
-import queryCors from "./components/queryWithCors";
 import Section from "./components/Section";
-// import Photo1 from "./Public/Image/students.jpg";
 
 const App = () => {
-  const APIList = "https://www.udemy.com/api-2.0/courses/";
-  const APIClient = "gIJdINdl183ge1sMAIWh2aaCaDCyq50y3SXIW9sJ";
-  const APISecret =
-    "Ocxd4ix5nIfwn8GhnA96O74O4NaxmzCfxdxsJps9RPAFI3gDJvd5WXTzDixzixWwQxfyg3CuaP0rG6zmdzTqKHf2mC8qDZIJrGotNnUp4cxdBrNuZkY4ZLKFAEmW5bLZ";
-  useEffect(() => {
-    const query = Query(APIList);
-    console.log("Query : ", query);
-    const query2 = queryCors(APIList, APIClient, APISecret);
-    console.log("Query : ", query2);
-  }, []);
+  // const APIList = "https://www.udemy.com/api-2.0/courses/";
+  // const APIClient = "gIJdINdl183ge1sMAIWh2aaCaDCyq50y3SXIW9sJ";
+  // const APISecret =
+  //   "Ocxd4ix5nIfwn8GhnA96O74O4NaxmzCfxdxsJps9RPAFI3gDJvd5WXTzDixzixWwQxfyg3CuaP0rG6zmdzTqKHf2mC8qDZIJrGotNnUp4cxdBrNuZkY4ZLKFAEmW5bLZ";
+  // useEffect(() => {
+  //   const query = Query(APIList);
+  //   console.log("Query : ", query);
+  //   const query2 = queryCors(APIList, APIClient, APISecret);
+  //   console.log("Query : ", query2);
+  // }, []);
+
   return (
     <>
       <Header />
